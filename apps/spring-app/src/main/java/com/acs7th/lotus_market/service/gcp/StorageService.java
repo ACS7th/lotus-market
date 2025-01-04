9eae67ef28ae4858d949c575c35185c4f534c255
dealim <dealimmmm@gmail.com>
2025-01-04 20:29:25 +0900

    chore: rm trash files

--- apps/spring-app/src/main/java/com/acs7th/lotus_market/service/gcp/StorageService.java
+++ apps/spring-app/src/main/java/com/acs7th/lotus_market/service/gcp/StorageService.java
@@ -31,12 +31,7 @@ public class StorageService {
                 file.getInputStream()
             );
 
-<<<<<<< HEAD:back-ns/spring-lotus-market/src/main/java/com/acs7th/lotus_market/service/gcp/StorageService.java
             imageUrl = String.format("https://storage.googleapis.com/%s/%s", bucketName, uniqueFilename);
-
-=======
-            imageUrl = String.format("https://kimjichang.site/%s", uniqueFilename);
->>>>>>> ca4898ee8ba9e7a1c2c62d2bfbf72b3c7b7336ad:apps/spring-lotus-market/src/main/java/com/acs7th/lotus_market/service/gcp/StorageService.java
         }
 
         return imageUrl;
