926ee329b70a5d09607f9fd2738aa03cdbb7c29e
dealim <dealimmmm@gmail.com>
2025-01-04 12:11:54 +0900

    refactor: change dir

--- apply-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find . -type d -name "k8s" | while read -r k8s_dir; do
-  echo "Applying resources in: $k8s_dir"
-  kubectl apply -f "$k8s_dir"
-done
-
-echo "All Kubernetes resources have been applied."
